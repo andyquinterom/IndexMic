1dd9168eff342e532c77fbb360d34811a7ecddae
andres <afquinteromoreano@gmail.com>
2021-08-28 03:26:05 -0500

    fix: Corrige errores con instalación

--- install.sh
+++ install.sh
@@ -4,16 +4,31 @@ read -p "¿Su distribción utiliza RPM (1) o DEB (2)?" pm
 case $pm in
     [1]* )
         wget -O shinyproxy.rpm https://www.shinyproxy.io/downloads/shinyproxy_2.5.0_x86_64.rpm;
-        sudo dpkg -i shinyproxy.deb;;
+        sudo rpm -i shinyproxy.rpm;;
     [2]* )
         wget -O shinyproxy.deb https://www.shinyproxy.io/downloads/shinyproxy_2.5.0_amd64.deb;
-        sudo rpm -i shinyproxy.rpm;;
+        sudo dpkg -i shinyproxy.deb;;
     * ) echo "Por favor ingrese 1 o 2."; exit 0;;
 esac
 
 sudo cp shinyproxy/application.yml /etc/shinyproxy/application.yml
 sudo cp -R shinyproxy/templates /etc/shinyproxy/
 
-sudo systemctl enable shinyproxy
+read -p "¿Desea activar el servicio con systemd? sí (s)/ no (n)" yn
+case $yn in
+    [Ss]* )
+        sudo systemctl enable shinyproxy;;
+    [Nn]* )
+        echo "Utilice 'systemctl enable shinyproxy' para correr IndexMic.";;
+    * ) echo "Por favor ingrese sí (s) o no (n)."; exit 0;;
+esac
 
-echo "Utilice 'systemctl start shinyproxy' para correr IndexMic."
+read -p "¿Desea iniciar el servicio con systemd? sí (s)/ no (n)" yn
+case $yn in
+    [Ss]* )
+        sudo systemctl start shinyproxy;;
+    [Nn]* )
+        echo "Utilice 'systemctl start shinyproxy' para correr IndexMic.";;
+    * ) echo "Por favor ingrese sí (s) o no (n)."; exit 0;;
+esac
+
